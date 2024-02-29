322130d765b37508ead610568c421b57629dc4d2
PEDRO HENRIQUE SILVINO VIEIRA <1A231006164@fumecacad.local>
2024-02-29 08:09:10 -0300

    Enviando arquivos

---
+++ ExercicioNivelamento/src/ExercicioNivelamento.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : ExercicioNivelamento.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL); //Limpa a saída de vídeo
+
+
+	return 0;
+}
